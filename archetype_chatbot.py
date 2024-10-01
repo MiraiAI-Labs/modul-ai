50b92eb948eb1d18cf8727a2a594ec2d4c6e8405
Rangga <ranggaaldosas@gmail.com>
2024-10-01 21:36:23 +0700

    feat: fixing upskill-judge endpoint

--- archetype_chatbot.py
+++ archetype_chatbot.py
@@ -1,10 +1,11 @@
-import os
-import google.generativeai as genai
 import json
+
+import google.generativeai as genai
 import yaml
 
 with open("./config.yaml", "r") as file:
     config = yaml.safe_load(file)
+
 
 class ArchetypeChatbot:
     def __init__(self, configs=config):
@@ -16,9 +17,9 @@ class ArchetypeChatbot:
             "max_output_tokens": 8192,
             "response_mime_type": "text/plain",
         }
-        
+
         self.pointer = 0
-    
+
     def pick_random_key(self):
         if self.api_key:
             pair_api_key = self.api_key[self.pointer]
@@ -28,19 +29,29 @@ class ArchetypeChatbot:
             return api_key
         else:
             return "No more API keys available."
-    
+
     def process_text(self, input_text):
         api_key = self.pick_random_key()
         genai.configure(api_key=api_key)
         model = genai.GenerativeModel(
             model_name="gemini-1.5-pro",
             generation_config=self.gen_config,
-            system_instruction="Cek apakah untuk data ini, jawaban user sudah sesuai dengan kunci jawaban di setiap soalnya. Beri juga persentase kemiripan atau keterkaitan (nilai anda) jawaban user terhadap kunci jawabannya. Apabila persentase di bawah 70% DAN jawabannya tidak sesuai menurut Anda (dengan logis tentu saja), beri penjelasan atau jawaban yang seharusnya. Respon anda harus dalam bentuk JSON, dengan bentuk:\n\n{\"Soal\": ...,\n \"Nilai\": ..., # Skalanya dari 0-100\n \"Komentar\":...\n}\n\nJika ada lebih dari satu soal, maka seperti biasa,\n\n{{\"id\": ...,\n \"nilai\": ..., # Skalanya dari 0-100\n \"komentar\":...\n},\n{\"id\": ...,\n \"nilai\": ..., # Skalanya dari 0-100\n \"komentar\":...\n},\n...\n}\n\nGunakan bahasa yang dapat meng-encourage user, terkadang beri semangat kepada user agar tetap bersemangat dalam meningkatkan kemampuan dirinya. Anda tidak diperbolehkan menjawab hal diluar konteks ini, pastikan supaya jawaban anda hanya dalam bentuk JSON.",
+            system_instruction='Cek apakah untuk data ini, jawaban user sudah sesuai dengan kunci jawaban di setiap soalnya. Beri juga persentase kemiripan atau keterkaitan (nilai anda) jawaban user terhadap kunci jawabannya. Apabila persentase di bawah 70% DAN jawabannya tidak sesuai menurut Anda (dengan logis tentu saja), beri penjelasan atau jawaban yang seharusnya. Respon anda harus dalam bentuk JSON, dengan bentuk:\n\n{"Soal": ...,\n "Nilai": ..., # Skalanya dari 0-100\n "Komentar":...\n}\n\nJika ada lebih dari satu soal, maka seperti biasa,\n\n{{"id": ...,\n "nilai": ..., # Skalanya dari 0-100\n "komentar":...\n},\n{"id": ...,\n "nilai": ..., # Skalanya dari 0-100\n "komentar":...\n},\n...\n}\n\nGunakan bahasa yang dapat meng-encourage user, terkadang beri semangat kepada user agar tetap bersemangat dalam meningkatkan kemampuan dirinya. Anda tidak diperbolehkan menjawab hal diluar konteks ini, pastikan supaya jawaban anda hanya dalam bentuk JSON.',
         )
-        
+
         chat_session = model.start_chat(history=[])
         response = chat_session.send_message(input_text)
 
+        print(f"Raw response from AI: {response.text}")
+
         response_text = response.text.replace("```json", "").replace("```", "").strip()
-        response_text = json.loads(response_text)
+
+        print(f"Cleaned response: {response_text}")
+
+        try:
+            response_text = json.loads(response_text)
+        except json.JSONDecodeError as e:
+            print(f"Error decoding JSON: {e}")
+            raise
+
         return response_text
--- main.py
+++ main.py
@@ -1,55 +1,59 @@
 import csv
+import hashlib
+import hmac
 import json
+import os
+import uuid
 from pathlib import Path
+from typing import Annotated, List
 
 import numpy as np
+import requests
 import uvicorn
-from analyst import Analyzer
-from cv_analyst import GeminiCVAnalyst
-from archetype_chatbot import ArchetypeChatbot
-from fastapi import FastAPI, File, UploadFile, BackgroundTasks, Form
+from dotenv import load_dotenv
+from fastapi import BackgroundTasks, FastAPI, Form, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
 from jobspy import scrape_jobs
 from pydantic import BaseModel
-import uuid
-from fastapi.staticfiles import StaticFiles
-from dotenv import load_dotenv
-import os
-import requests
-import hmac
-import hashlib
-from typing import Annotated
+
+from analyst import Analyzer
+from archetype_chatbot import ArchetypeChatbot
+from cv_analyst import GeminiCVAnalyst
+
 
 def convert_int64(o):
     if isinstance(o, np.int64):
         return int(o)
     raise TypeError
 
+
 load_dotenv()
+
 
 def send_webhook(event, data):
     secret = os.getenv("WEBHOOK_SECRET", "very-long-secret")
     url = os.getenv("WEBHOOK_URL", "http://localhost:8002/webhook")
 
-    payload = {
-        "event": event,
-        "data": data
-    }
+    payload = {"event": event, "data": data}
 
     payload_string = json.dumps(payload)
     payload_bytes = json.dumps(payload).encode("utf-8")
-    
+
     secret_bytes = bytes(secret, "utf-8")
-    signature = hmac.new( secret_bytes, payload_bytes, hashlib.sha256 )
+    signature = hmac.new(secret_bytes, payload_bytes, hashlib.sha256)
 
     headers = {
         "Content-Type": "application/json",
         "Signature": signature.hexdigest(),
     }
 
-    print(f"Sending webhook to {url} with payload {payload_string} and headers {headers}")
+    print(
+        f"Sending webhook to {url} with payload {payload_string} and headers {headers}"
+    )
 
     response = requests.post(url, data=payload_string, headers=headers)
+
 
 app = FastAPI()
 app.add_middleware(
@@ -65,15 +69,15 @@ public_dir = Path(__file__).parent / "public"
 if not public_dir.exists():
     raise RuntimeError(f"Directory '{public_dir}' does not exist")
 
-app.mount(
-    "/public", StaticFiles(directory=public_dir), name="public"
-)
+app.mount("/public", StaticFiles(directory=public_dir), name="public")
+
 
 class TextSubmission(BaseModel):
     text: str
     jobs_analysis_id: int
     job_lists_id: int
     mode: str = "default"
+
 
 def analyze_task(submission: TextSubmission):
     print(f"Received submission: {submission.text}")
@@ -89,7 +93,9 @@ def analyze_task(submission: TextSubmission):
     print(f"Found {len(jobs)} jobs")
 
     jobs_file_name = "public/" + str(uuid.uuid4()) + ".csv"
-    jobs.to_csv(jobs_file_name, quoting=csv.QUOTE_NONNUMERIC, escapechar="\\", index=False)
+    jobs.to_csv(
+        jobs_file_name, quoting=csv.QUOTE_NONNUMERIC, escapechar="\\", index=False
+    )
 
     print(f"Saved jobs to {jobs_file_name}")
 
@@ -124,13 +130,15 @@ def analyze_task(submission: TextSubmission):
         "jobs_analysis_id": submission.jobs_analysis_id,
         "job_lists_id": submission.job_lists_id,
     }
-    
+
     send_webhook("analysis_generated", response)
+
 
 @app.post("/generate_analysis")
 async def analyze(submission: TextSubmission, background_tasks: BackgroundTasks):
     background_tasks.add_task(analyze_task, submission)
     return {"message": "Analysis started"}
+
 
 def analyze_cv_task(input_text, json_data, review_id: str):
     cv_analyst = GeminiCVAnalyst()
@@ -140,35 +148,79 @@ def analyze_cv_task(input_text, json_data, review_id: str):
         "result": result,
         "review_id": review_id,
     }
-    
+
     send_webhook("cv_analyzed", response)
 
+
 @app.post("/analyze_cv")
-async def analyze_cv(background_tasks: BackgroundTasks, file: UploadFile, job_analysis: UploadFile, review_id: Annotated[str, Form()]):
-    print(f"Received CV file: {file.filename} and job analysis file: {job_analysis.filename} with review_id: {review_id}")
+async def analyze_cv(
+    background_tasks: BackgroundTasks,
+    file: UploadFile,
+    job_analysis: UploadFile,
+    review_id: Annotated[str, Form()],
+):
+    print(
+        f"Received CV file: {file.filename} and job analysis file: {job_analysis.filename} with review_id: {review_id}"
+    )
     input_text = await file.read()
     json_data = json.load(job_analysis.file)
     background_tasks.add_task(analyze_cv_task, input_text, json_data, review_id)
     return {"message": "CV analysis started"}
 
+
 #################################### Bagian ini baru ####################################################
+class QuizItem(BaseModel):
+    question: str
+    answer: str
+    userAnswer: str
 
-def archetype_quiz_judge_task(text, review_id: str): # Sementara aku set review_id dulu ngikutin atas
-    judge = ArchetypeChatbot()
-    res = judge.process_text(text)
-    
-    resp = {
-        "result": res,
-        "review_id": review_id
-    }
 
-    send_webhook("archetype_quiz_result", resp)
+class QuizResult(BaseModel):
+    feedback: str
+    nilai: float
 
-@app.post("/archetype_quiz_judge")
-async def analyze_cv(background_tasks: BackgroundTasks, input_text : TextSubmission, review_id: Annotated[str, Form()]):
-    # print(f"Received CV file: {file.filename} with review_id: {review_id}")
-    background_tasks.add_task(archetype_quiz_judge_task, input_text, review_id)
-    return {"message": "Archetype Evaluation started"}
+
+chatbot = ArchetypeChatbot()
+
+
+@app.post("/upskill-judge", response_model=List[QuizResult])
+async def upskill_judge(quiz_items: List[QuizItem]):
+    results = []
+    for item in quiz_items:
+        input_text = json.dumps(
+            {
+                "question": item.question,
+                "correct_answer": item.answer,
+                "user_answer": item.userAnswer,
+            }
+        )
+
+        processed_result = chatbot.process_text(input_text)
+
+        feedback = processed_result.get("Komentar", "Tidak ada feedback.")
+        nilai = processed_result.get("Nilai", 0)
+
+        results.append(QuizResult(feedback=feedback, nilai=nilai))
+
+    return results
+
+
+# def archetype_quiz_judge_task(
+#     text, review_id: str
+# ):  # Sementara aku set review_id dulu ngikutin atas
+#     judge = ArchetypeChatbot()
+#     res = judge.process_text(text)
+
+#     resp = {"result": res, "review_id": review_id}
+
+#     send_webhook("archetype_quiz_result", resp)
+
+
+# @app.post("/archetype_quiz_judge")
+# async def analyze_cv(background_tasks: BackgroundTasks, input_text : TextSubmission, review_id: Annotated[str, Form()]):
+#     # print(f"Received CV file: {file.filename} with review_id: {review_id}")
+#     background_tasks.add_task(archetype_quiz_judge_task, input_text, review_id)
+#     return {"message": "Archetype Evaluation started"}
 
 #############################################################################################################
 
